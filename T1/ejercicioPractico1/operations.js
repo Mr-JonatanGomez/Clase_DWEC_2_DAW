533156df1b74bff5615df64dd9ce7ca15ee87ec8
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-16 12:06:05 +0200

    exercice of nodeJS & test

---
+++ T1/ejercicioPractico1/operations.js
@@ -0,0 +1,20 @@
+function suma(a, b) {
+  return a + b
+}
+function resta(a, b) {
+  return a - b
+}
+function multiplicacion(a, b) {
+  return a * b
+}
+function division(a, b) {
+  return a / b
+}
+/* Export functions, para que esten a disposicion del resto de funciones*/
+
+module.export = {
+  suma,
+  resta,
+  multiplicacion,
+  division
+}
